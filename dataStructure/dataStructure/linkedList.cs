d1772aa3cea411de42b433614f0b4ff4b6852068
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-30 18:07:28 +0530

    [Nantha] - list created and data added

---
+++ dataStructure/dataStructure/linkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace dataStructure
+{
+    internal class linkedList
+    {
+        internal node head;
+        public void add(int id)
+        {
+            node node = new node(id);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted in to linked list",node.id);
+        }
+    }
+}
---
+++ dataStructure/dataStructure/mainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataStructure
+{
+    internal class mainClass
+    {
+        public static void Main(string[] args)
+        {
+            linkedList list = new linkedList();
+            list.add(56);
+            list.add(30);
+            list.add(70);
+        }
+    }
+}
