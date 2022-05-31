1a29365211b84a05d0ce9c30400e81b0b4d2ca16
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-31 18:36:01 +0530

    [Nantha] - Simple Stack Created

---
+++ dataStructure/dataStructure/linkedListStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataStructure
+{
+    internal class linkedListStack
+    {
+        public node top;
+        public linkedListStack()
+        {
+            this.top = null;
+        }
+        public void addStack(int data)
+        {
+            node node = new node(data);
+            if (this.top == null)
+            {
+                this.top = node;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine(node.data +" Added to stack");
+
+        }
+        public void display()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                node temp = this.top;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;   
+                }
+            }
+        }
+        
+    }
+}
--- dataStructure/dataStructure/mainClass.cs
+++ dataStructure/dataStructure/mainClass.cs
@@ -11,66 +11,86 @@ namespace dataStructure
     {
         public static void Main(string[] args)
         {
-            dataStructure.linkedList run = new dataStructure.linkedList();
-            linkedList list = new linkedList();
-            Console.WriteLine("1 to create simple linked list");
-            Console.WriteLine("2 to swap position");
-            Console.WriteLine("3 to Perform Append");
-            Console.WriteLine("4 to Insert Value");
-            Console.WriteLine("5 to Remove First Value");
-            Console.WriteLine("6 to Remove Last Value");
-            int select = Convert.ToInt32(Console.ReadLine());
-            switch (select)
+            Console.WriteLine("1 for Linked List");
+            Console.WriteLine("2 for Stack");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
             {
                 case 1:
-                    list.add(56);
-                    list.add(30);
-                    list.add(70);
-                    list.display();
+                    dataStructure.linkedList run = new dataStructure.linkedList();
+                    linkedList list = new linkedList();
+                    Console.WriteLine("1 to create simple linked list");
+                    Console.WriteLine("2 to swap position");
+                    Console.WriteLine("3 to Perform Append");
+                    Console.WriteLine("4 to Insert Value");
+                    Console.WriteLine("5 to Remove First Value");
+                    Console.WriteLine("6 to Remove Last Value");
+                    int select = Convert.ToInt32(Console.ReadLine());
+                    switch (select)
+                    {
+                        case 1:
+                            list.add(56);
+                            list.add(30);
+                            list.add(70);
+                            list.display();
+                            break;
+                        case 2:
+                            list.swapValues(56);
+                            list.swapValues(30);
+                            list.swapValues(70);
+                            list.display();
+                            break;
+                        case 3:
+                            list.add(56);
+                            list.add(30);
+                            list.add(70);
+                            list.display();
+                            break;
+                        case 4:
+                            list.add(56);
+                            list.add(70);
+                            Console.WriteLine("Before insert");
+                            list.display();
+                            Console.WriteLine("After insert");
+                            list.insertNodeInbetween(2, 30);
+                            list.display();
+                            break;
+                        case 5:
+                            list.add(56);
+                            list.add(30);
+                            list.add(70);
+                            Console.WriteLine("--------------------");
+                            Console.WriteLine("Values Present in List");
+                            list.display();
+                            list.removeFirst();
+                            Console.WriteLine("After Remove First Value");
+                            list.display();
+                            break;
+                        case 6:
+                            list.add(56);
+                            list.add(30);
+                            list.add(70);
+                            Console.WriteLine("--------------------");
+                            Console.WriteLine("Values Present in List");
+                            list.display();
+                            list.removeLastValue();
+                            Console.WriteLine("After Remove Last Value");
+                            list.display();
+                            break;
+                    }
                     break;
                 case 2:
-                    list.swapValues(56);
-                    list.swapValues(30);
-                    list.swapValues(70);
-                    list.display();
+                    //dataStructure.linkedListStack runStack = new linkedListStack();
+                    linkedListStack linkListstack = new linkedListStack();
+                    linkListstack.addStack(70);
+                    linkListstack.addStack(30);
+                    linkListstack.addStack(56);
+                    linkListstack.display();
+                    
                     break;
-                case 3:
-                    list.add(56);
-                    list.add(30);
-                    list.add(70);
-                    list.display();
-                    break;
-                case 4:
-                    list.add(56);
-                    list.add(70);
-                    Console.WriteLine("Before insert");
-                    list.display();
-                    Console.WriteLine("After insert");
-                    list.insertNodeInbetween(2, 30);
-                    list.display();
-                    break;
-                case 5:
-                    list.add(56);
-                    list.add(30);
-                    list.add(70);
-                    Console.WriteLine("--------------------");
-                    Console.WriteLine("Values Present in List");
-                    list.display();
-                    list.removeFirst();
-                    Console.WriteLine("After Remove First Value");
-                    list.display();
-                    break;
-                case 6:
-                    list.add(56);
-                    list.add(30);
-                    list.add(70);
-                    Console.WriteLine("--------------------");
-                    Console.WriteLine("Values Present in List");
-                    list.display();
-                    list.removeLastValue();
-                    Console.WriteLine("After Remove Last Value");
-                    list.display();
-                    break;
+            
+                    
+
 
             }
            
